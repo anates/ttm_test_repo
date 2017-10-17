abda95d5f2d76c794e884f89ce30e2b46ba5bbad
Roland Richter <Roland.Richter2@gmx.de>
2017-10-17 13:29:49 +0200

    Added necessary files

---
+++ compile.sh
@@ -0,0 +1,1 @@
+rm -rf *.*tu* && make clean && clear && make release && clear && /opt/openmpi/bin/mpirun -n $1 main
---
+++ compile_debug.sh
@@ -0,0 +1,1 @@
+rm -rf *.*tu* && make clean && clear && make debug && clear && /opt/openmpi/bin/mpirun -n 1 main parameters.prm 
